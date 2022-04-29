6e46ba45f1bc072a66e71d5119ce602185747993
Huy Truong <huy.truong@shapeandshift.dev>
2022-04-29 15:12:46 +0700

    CC-89 - Giropay integration

---
+++ src/Handler/Method/GiropayHandler.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types=1);
+
+namespace CheckoutCom\Shopware6\Handler\Method;
+
+use Checkout\Common\PaymentSourceType;
+use Checkout\Payments\PaymentRequest;
+use Checkout\Payments\Source\Apm\RequestGiropaySource;
+use CheckoutCom\Shopware6\Handler\PaymentHandler;
+use Shopware\Core\Checkout\Order\OrderEntity;
+use Shopware\Core\Framework\Validation\DataBag\RequestDataBag;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+
+class GiropayHandler extends PaymentHandler
+{
+    public function getSnippetKey(): string
+    {
+        return 'checkoutCom.paymentMethod.giropayLabel';
+    }
+
+    public static function getPaymentMethodType(): string
+    {
+        return PaymentSourceType::$giropay;
+    }
+
+    public function prepareDataForPay(
+        PaymentRequest $paymentRequest,
+        RequestDataBag $dataBag,
+        OrderEntity $order,
+        SalesChannelContext $context
+    ): PaymentRequest {
+        $this->enableThreeDsRequest($paymentRequest);
+
+        $paymentRequest->source = $this->buildGiropaySource($order);
+
+        return $paymentRequest;
+    }
+
+    /**
+     * Build token source to call the Checkout.com API
+     */
+    private function buildGiropaySource(OrderEntity $order): RequestGiropaySource
+    {
+        $request = new RequestGiropaySource();
+        $request->purpose = \sprintf('order_%s', $order->getOrderNumber());
+
+        return $request;
+    }
+}
