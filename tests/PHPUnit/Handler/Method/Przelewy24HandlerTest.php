bb9f99f04be71001162a004065b651c4b81d0949
Huy Truong <huy.truong@shapeandshift.dev>
2022-05-10 14:33:13 +0700

    CC-91 - Przelewy24 integration

---
+++ tests/PHPUnit/Handler/Method/Przelewy24HandlerTest.php
@@ -0,0 +1,81 @@
+<?php
+declare(strict_types=1);
+
+namespace CheckoutCom\Shopware6\Tests\Handler\Method;
+
+use Checkout\Payments\PaymentRequest;
+use CheckoutCom\Shopware6\Handler\Method\Przelewy24Handler;
+use CheckoutCom\Shopware6\Handler\PaymentHandler;
+use CheckoutCom\Shopware6\Handler\Source\RequestPrzelewy24Source;
+use CheckoutCom\Shopware6\Service\CheckoutApi\CheckoutTokenService;
+use CheckoutCom\Shopware6\Service\Extractor\OrderExtractor;
+use CheckoutCom\Shopware6\Service\LoggerService;
+use CheckoutCom\Shopware6\Tests\Handler\AbstractPaymentHandlerTest;
+use Shopware\Core\Checkout\Order\Aggregate\OrderCustomer\OrderCustomerEntity;
+use Shopware\Core\Checkout\Order\OrderEntity;
+use Shopware\Core\Framework\Validation\DataValidator;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class Przelewy24HandlerTest extends AbstractPaymentHandlerTest
+{
+    protected PaymentHandler $paymentHandler;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->paymentHandler = new Przelewy24Handler(
+            $this->createMock(LoggerService::class),
+            $this->createMock(TranslatorInterface::class),
+            $this->createMock(DataValidator::class),
+            $this->createMock(OrderExtractor::class),
+            $this->createMock(CheckoutTokenService::class),
+            $this->paymentPayFacade,
+            $this->paymentFinalizeFacade
+        );
+    }
+
+    public function testSnippetKey(): void
+    {
+        static::assertSame('checkoutCom.paymentMethod.p24Label', $this->paymentHandler->getSnippetKey());
+    }
+
+    public function testPrepareDataForPay(): void
+    {
+        $dataBag = $this->getRequestBag();
+        $order = new OrderEntity();
+        $order->setId('testId');
+        $order->setOrderNumber('test');
+        $orderCustomer = new OrderCustomerEntity();
+        $orderCustomer->setId('testId2');
+        $orderCustomer->setFirstName('Firstname');
+        $orderCustomer->setLastName('Lastname');
+        $orderCustomer->setEmail('test@example.com');
+        $order->setOrderCustomer($orderCustomer);
+        $paymentRequest = $this->paymentHandler->prepareDataForPay(
+            $this->createMock(PaymentRequest::class),
+            $dataBag,
+            $order,
+            $this->saleChannelContext
+        );
+
+        static::assertInstanceOf(PaymentRequest::class, $paymentRequest);
+        static::assertInstanceOf(RequestPrzelewy24Source::class, $paymentRequest->source);
+    }
+
+    public function testPrepareDataForPayWithException(): void
+    {
+        static::expectException(\Exception::class);
+
+        $dataBag = $this->getRequestBag();
+        $order = new OrderEntity();
+        $order->setId('testId');
+        $order->setOrderNumber('test');
+        $this->paymentHandler->prepareDataForPay(
+            $this->createMock(PaymentRequest::class),
+            $dataBag,
+            $order,
+            $this->saleChannelContext
+        );
+    }
+}
