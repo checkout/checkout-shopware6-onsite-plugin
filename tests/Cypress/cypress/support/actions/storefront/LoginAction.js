c375c31bfc2edbf1c1ae18f14042349ed6f2f43c
Vu Le <vu.le@shapeandshift.dev>
2022-08-25 21:36:14 +0700

    CC-177: Increate pageLoadTimeout

--- tests/Cypress/cypress/support/actions/storefront/LoginAction.js
+++ tests/Cypress/cypress/support/actions/storefront/LoginAction.js
@@ -14,8 +14,6 @@ class LoginAction {
         cy.get('#loginPassword').typeAndCheckStorefront(password);
         cy.get('.login-submit [type="submit"]').click();
 
-        cy.screenshot('login screen');
-
         cy.visit('/');
     }
 }
