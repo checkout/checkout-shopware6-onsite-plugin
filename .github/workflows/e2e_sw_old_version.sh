58633f0320e0ad47990fec2d5612d2da18b805af
Hung Mac <hung@shapeandshift.dev>
2022-08-29 11:12:54 +0700

    CC-163 - change max parallel to 3

--- .github/workflows/e2e_sw_old_version.sh
+++ .github/workflows/e2e_sw_old_version.sh
@@ -9,9 +9,9 @@ else
 fi
 
 docker cp shop:/var/www/html/config/bundles.php $(pwd)/bundles.php
-if [[ "${1}" == "6.4.7.0" ]] || [[ "${1}" == "6.4.6.1" ]] || [[ "${1}" == "6.4.5.1" ]]; then
+if [[ "${1}" == "6.4.7.0" ]] || [[ "${1}" == "6.4.6.1" ]]; then
   sed -i'' -e 's/return $bundles;/if (\\class_exists(Shopware\\Core\\DevOps\\DevOps::class)) {$bundles[Shopware\\Core\\DevOps\\DevOps::class] = ["all" => true];} return $bundles;/g' bundles.php
-elif [[ "${1}" == "6.4.4.1" ]] || [[ "${1}" == "6.4.3.1" ]]; then
+elif [[ "${1}" == "6.4.5.1" ]] || [[ "${1}" == "6.4.4.1" ]] || [[ "${1}" == "6.4.3.1" ]]; then
   sed -i'' -e 's/];/Shopware\\Core\\DevOps\\DevOps::class => ["all" => true]];/g' bundles.php
 fi
 docker cp $(pwd)/bundles.php shop:/var/www/html/config/bundles.php
