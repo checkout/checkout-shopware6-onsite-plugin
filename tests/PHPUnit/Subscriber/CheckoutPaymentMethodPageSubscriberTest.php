a00d6ee22343a2c808983c717003b8a263f5c017
Khoa Pham <khoa.pham@shapeandshift.dev>
2022-09-06 16:31:39 +0700

    CC-181: Fix residual code in payment method subscriber

--- tests/PHPUnit/Subscriber/CheckoutPaymentMethodPageSubscriberTest.php
+++ tests/PHPUnit/Subscriber/CheckoutPaymentMethodPageSubscriberTest.php
@@ -3,21 +3,16 @@
 namespace CheckoutCom\Shopware6\Tests\Subscriber;
 
 use CheckoutCom\Shopware6\Handler\Method\CardPaymentHandler;
-use CheckoutCom\Shopware6\Handler\PaymentHandler;
-use CheckoutCom\Shopware6\Service\Cart\AbstractCartService;
 use CheckoutCom\Shopware6\Service\CustomerService;
-use CheckoutCom\Shopware6\Service\PaymentMethodService;
 use CheckoutCom\Shopware6\Subscriber\CheckoutPaymentMethodPageSubscriber;
 use CheckoutCom\Shopware6\Tests\Traits\ContextTrait;
 use CheckoutCom\Shopware6\Tests\Traits\OrderTrait;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
-use Shopware\Core\Checkout\Customer\Aggregate\CustomerAddress\CustomerAddressEntity;
 use Shopware\Core\Checkout\Customer\CustomerEntity;
 use Shopware\Core\Checkout\Payment\PaymentMethodCollection;
 use Shopware\Core\Checkout\Payment\PaymentMethodEntity;
 use Shopware\Core\Framework\Uuid\Uuid;
-use Shopware\Core\System\Country\CountryEntity;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Shopware\Storefront\Page\Account\Order\AccountEditOrderPage;
 use Shopware\Storefront\Page\Account\Order\AccountEditOrderPageLoadedEvent;
@@ -33,16 +28,6 @@ class CheckoutPaymentMethodPageSubscriberTest extends TestCase
     use OrderTrait;
 
     /**
-     * @var PaymentMethodService|MockObject
-     */
-    private $paymentMethodService;
-
-    /**
-     * @var AbstractCartService|MockObject
-     */
-    private $cartService;
-
-    /**
      * @var MockObject|SalesChannelContext
      */
     private $salesChannelContext;
@@ -51,12 +36,7 @@ class CheckoutPaymentMethodPageSubscriberTest extends TestCase
 
     public function setUp(): void
     {
-        $this->paymentMethodService = $this->createMock(PaymentMethodService::class);
-        $this->cartService = $this->createMock(AbstractCartService::class);
-        $this->subscriber = new CheckoutPaymentMethodPageSubscriber(
-            $this->paymentMethodService,
-            $this->cartService,
-        );
+        $this->subscriber = new CheckoutPaymentMethodPageSubscriber();
         $this->salesChannelContext = $this->getSaleChannelContext($this);
     }
 
@@ -78,13 +58,6 @@ class CheckoutPaymentMethodPageSubscriberTest extends TestCase
 
         $this->salesChannelContext->method('getCustomer')
             ->willReturn($customer);
-
-        $this->paymentMethodService->method('getPaymentHandlersByHandlerIdentifier')
-            ->willReturn(
-                $this->createConfiguredMock(PaymentHandler::class, [
-                    'getAvailableCountries' => [],
-                ])
-            );
 
         $event = new CheckoutConfirmPageLoadedEvent(
             $page,
@@ -111,13 +84,6 @@ class CheckoutPaymentMethodPageSubscriberTest extends TestCase
         $this->salesChannelContext->method('getCustomer')
             ->willReturn($customer);
 
-        $this->paymentMethodService->method('getPaymentHandlersByHandlerIdentifier')
-            ->willReturn(
-                $this->createConfiguredMock(PaymentHandler::class, [
-                    'getAvailableCountries' => [],
-                ])
-            );
-
         $event = new AccountEditOrderPageLoadedEvent(
             $page,
             $this->salesChannelContext,
@@ -142,13 +108,6 @@ class CheckoutPaymentMethodPageSubscriberTest extends TestCase
 
         $this->salesChannelContext->method('getCustomer')
             ->willReturn($customer);
-
-        $this->paymentMethodService->method('getPaymentHandlersByHandlerIdentifier')
-            ->willReturn(
-                $this->createConfiguredMock(PaymentHandler::class, [
-                    'getAvailableCountries' => [],
-                ])
-            );
 
         $event = new AccountPaymentMethodPageLoadedEvent(
             $page,
@@ -184,16 +143,6 @@ class CheckoutPaymentMethodPageSubscriberTest extends TestCase
                 ],
             ],
         ]);
-
-        $customer->setActiveBillingAddress(
-            $this->createConfiguredMock(CustomerAddressEntity::class, [
-                'getId' => 'foo',
-                'getCountry' => $this->createConfiguredMock(CountryEntity::class, [
-                    'getId' => 'foo',
-                    'getIso' => 'foo',
-                ]),
-            ])
-        );
 
         return $customer;
     }
